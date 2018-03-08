435d61d2ac8cedb4016d5fcb7f313905c092870d
고현민 <junseol86@gmail.com>
2018-03-09 02:36:38 +0900

    Add moving adjust handles

--- src/components/Popup/MypagePopup.vue
+++ src/components/Popup/MypagePopup.vue
@@ -12,6 +12,23 @@
               {{shapeAdj}}<br>
               {{shapeNick}}<br>
             </div>
+            <div id="colorLabels">
+              <div :style="colorLabelStyle" v-for="(color, idx) in ['R', 'G', 'B']" :key="idx">{{color}}</div>
+            </div>
+            <div id="colorAdjusts">
+              <div :style="colorAdjustStyle" v-for="(color, idx) in ['r', 'g', 'b']" :key="idx">
+                <div :style="colorAdjustInnerStyle"
+                @mousedown="setAjMoving($event, true)" @mouseup="setAjMoving($event, false)" @mouseleave="setAjMoving($event, false)"
+                @mousemove="ajMove($event, idx)"
+                >
+                  <img :src="`http://13.125.24.19:8001/interface/color_adjust_${color}.png`"/>
+                  <img :style="adjHandleStyle(idx)" class="abs handle" src="../../assets/img/color_adjust_handle.png"/>
+                  <div class="abs" :style="colorAdjustLabelStyle">
+                    {{colors[idx]}}
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -26,19 +43,49 @@ export default {
   name: 'MypagePopup',
   data () {
     return {
+      colors: [0, 0, 0],
+      ajMoving: false
+    }
+  },
+  methods: {
+    setAjMoving (e, onOff) {
+      this.ajMoving = onOff
+    },
+    ajMove (e, idx) {
+      if (!this.ajMoving) return
+      var x = e.layerX - this.adjRadius.r
+      var y = this.adjRadius.r - e.layerY
+      var rad = Math.atan(y / -x)
+      rad += rad < 0 ? Math.PI : 0
+      // var deg = rad * 180 / Math.PI
+      this.$set(this.colors, idx, Math.round(rad * 256 / Math.PI))
+    },
+    adjHandleStyle (idx) {
+      var rad = this.colors[idx] * 180 / 256 * (Math.PI / 180)
+      var x = this.adjRadius.r - (this.adjRadius.adjR * Math.cos(rad))
+      var y = this.adjRadius.r - (this.adjRadius.adjR * Math.sin(rad))
+      var handleSize = this.cw / 1600 * 28
+      console.log(`${x} ${y}`)
+      return {
+        width: handleSize + 'px',
+        height: handleSize + 'px',
+        left: x - (handleSize / 2) + 'px',
+        top: y - (handleSize / 2) + 'px'
+      }
     }
   },
   computed: {
+    cw () {
+      return parseInt(this.layout.centeredWidth.replace('px', ''))
+    },
     shapeSectionStyle () {
-      var cw = parseInt(this.layout.centeredWidth.replace('px', ''))
       return {
-        padding: `${100 * cw / 1600}px ${150 * cw / 1600}px`
+        padding: `${100 * this.cw / 1600}px ${150 * this.cw / 1600}px`
       }
     },
     shapeStyle () {
-      var cw = parseInt(this.layout.centeredWidth.replace('px', ''))
-      var length = cw * 320 / 1600 + 'px'
-      var radius = cw * 160 / 1600 + 'px'
+      var length = this.cw * 320 / 1600 + 'px'
+      var radius = this.cw * 160 / 1600 + 'px'
       var colorStr = this.state.account.color_str.split('-')
       return {
         width: length,
@@ -48,11 +95,10 @@ export default {
       }
     },
     shapeDescStyle () {
-      var cw = parseInt(this.layout.centeredWidth.replace('px', ''))
       return {
-        top: cw * 0.09 + 'px',
-        left: cw * 0.35 + 'px',
-        fontSize: cw / 550 + 'em'
+        top: this.cw * 0.09 + 'px',
+        left: this.cw * 0.35 + 'px',
+        fontSize: this.cw / 550 + 'em'
       }
     },
     shapeWB () {
@@ -64,7 +110,51 @@ export default {
     },
     shapeNick () {
       return this.$consts.shapes[this.state.account.shape][1]
+    },
+    colorLabelStyle () {
+      return {
+        fontSize: this.cw / 440 + 'em',
+        width: 400 * this.cw / 1600 + 'px',
+        marginTop: 64 * this.cw / 1600 + 'px',
+        marginBottom: 24 * this.cw / 1600 + 'px'
+      }
+    },
+    colorAdjustStyle () {
+      return {
+        width: 400 * this.cw / 1600 + 'px',
+        height: 164 * this.cw / 1600 + 'px'
+      }
+    },
+    colorAdjustInnerStyle () {
+      return {
+        width: 328 * this.cw / 1600 + 'px',
+        height: 164 * this.cw / 1600 + 'px',
+        marginLeft: 36 * this.cw / 1600 + 'px'
+      }
+    },
+    colorAdjustLabelStyle () {
+      return {
+        width: 328 * this.cw / 1600 + 'px',
+        height: 164 * this.cw / 1600 + 'px',
+        lineHeight: 240 * this.cw / 1600 + 'px',
+        top: 0,
+        fontSize: this.cw / 400 + 'em'
+      }
+    },
+    adjRadius () {
+      return {
+        // 마우스 반경의 반지름
+        r: 164 * this.cw / 1600,
+        // 반원 띠의 두께를 고려한 실제 핸들이 움직일 반지름
+        adjR: 145 * this.cw / 1600
+      }
     }
+  },
+  mounted () {
+    var colorsStrs = this.state.account.color_str.split('-')
+    this.$set(this.colors, 0, parseInt(colorsStrs[0]))
+    this.$set(this.colors, 1, parseInt(colorsStrs[1]))
+    this.$set(this.colors, 2, parseInt(colorsStrs[2]))
   }
 }
 </script>
